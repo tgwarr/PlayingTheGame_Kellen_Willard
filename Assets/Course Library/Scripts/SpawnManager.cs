5721dd822329e1cd36294e7e542b3077a7ae9fb7
Kellen <k.willard@hotmail.com>
2020-04-22 10:40:08 -0700

    4B

---
+++ Assets/Course Library/Scripts/SpawnManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+	public GameObject enemyPrefab;
+	private GameObject player;
+	private float randRange = 9;
+    // Start is called before the first frame update
+    void Start()
+    {
+		player = GameObject.Find("Player");
+		Instantiate(enemyPrefab, player.transform.position + GenSpawnPos(), enemyPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private Vector3 GenSpawnPos()
+	{
+		float spawnX = Random.Range(-randRange, randRange);
+		float spawnZ = Random.Range(-randRange, randRange);
+		Vector3 pos = new Vector3(spawnX, 0,spawnZ);
+		return pos;
+	}
+}
