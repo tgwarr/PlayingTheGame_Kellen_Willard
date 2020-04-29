3d8369743b1b65d6e277b97a746e13f5171070ac
Kellen <k.willard@hotmail.com>
2020-04-29 11:21:44 -0700

    4C

--- Assets/Course Library/Scripts/PlayerController.cs
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+	private Animator anim;
 	private Rigidbody playerRB; 
 	public float forSpeed;
 	private GameObject focalPoint;
 	private Renderer playerRend;
+	public bool hasPowerUp = false;
+	private float powerUpKick = 10.0f;
+	public GameObject powerUpRing;
 
     void Start()
     {
+		anim = GetComponent<Animator>();
 		playerRB = GetComponent<Rigidbody>();
 		playerRend = GetComponent<Renderer>();
 		focalPoint = GameObject.Find("Focal Point");
@@ -29,5 +34,37 @@ public class PlayerController : MonoBehaviour
 		{
 			playerRend.material.color = new Color(1 + verticalInput,1,1 + verticalInput);
 		}
+		powerUpRing.transform.position = transform.position;
     }
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("PowerUp"))
+		{
+			hasPowerUp = true;
+			anim.SetBool("HasPowerUp", true);
+			powerUpRing.SetActive(true);
+			Destroy(other.gameObject);
+			StartCoroutine(PowerUpCountdown());
+		}
+	}
+
+	private IEnumerator PowerUpCountdown()
+	{
+		yield return new WaitForSeconds(5.0f);
+		hasPowerUp = false;
+		anim.SetBool("HasPowerUp", false);
+		powerUpRing.SetActive(false);
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log("Player collided with " + collision.gameObject + " with powerUp set to " + hasPowerUp);
+		if(collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+		{
+			Rigidbody enemyRB = collision.gameObject.GetComponent<Rigidbody>();
+			Vector3 dir = collision.gameObject.transform.position - transform.position;
+			enemyRB.AddForce(dir * powerUpKick, ForceMode.Impulse);
+		}
+	}
 }
